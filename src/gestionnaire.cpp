24c30ce62591870e4b9fc0220342669e30c7b304
GabrielFontenelee <gabrielfontenele.ufc@gmail.com>
2025-03-18 11:27:39 +0100

    Start gestionnaire des ressources

---
+++ src/gestionnaire.cpp
@@ -0,0 +1,28 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <chrono>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <string>
+#include <thread>
+
+#include "../include/mapping.h"
+#include "../include/Tram.h"
+
+
+std::mutex R1, R2, R3, R4, R5;
+
+
+int main(nt argc, char *argv[]){
+
+
+    return 0;
+    
+}
